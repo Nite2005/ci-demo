4d89cd1a1056e338369e8181743b1b4e29e69c1b
Nite2005 <kumarnitesh140905@gmail.com>
2025-07-31 20:21:19 +0530

    linting issue

--- test_app.py
+++ test_app.py
@@ -3,13 +3,16 @@ from app import add, sub, mul
 
 class TestMathFunctions(unittest.TestCase):
 
+
     def test_add(self):
         self.assertEqual(add(4,5), 9)
         self.assertEqual(add(3,4), 7)
 
+
     def test_sub(self):
         self.assertEqual(sub(5,4), 1)
         self.assertEqual(sub(9,5), 4)
+
 
     def test_mul(self):
         self.assertEqual(mul(1, 2), 2)
